fca015045e66f9f87d8fa97a0eff83cd9c2c9140
ikramsaedi <ikramsaedi@gmail.com>
2022-09-07 22:18:26 +1000

    added in react router

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -3,9 +3,27 @@ import "@hotwired/turbo-rails";
 import "./controllers/hello_controller.js";
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-    return <h1>Hello World</h1>;
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>LOAFLOAFLOAF</p>
+        </>
+    );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Loaf() {
+    return <p>Loaf!!</p>;
+}
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="Loaf" element={<Loaf />} />
+        </Routes>
+    </BrowserRouter>,
+    document.getElementById("root"),
+);
