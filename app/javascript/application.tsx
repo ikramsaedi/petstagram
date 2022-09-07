785fd159b8c532b0eb5e2a35c6fff2a7b34ca3a3
ikramsaedi <ikramsaedi@gmail.com>
2022-09-07 21:02:20 +1000

    added react to petstagram!

--- app/javascript/application.tsx
+++ app/javascript/application.tsx
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "./controllers/hello_controller.js";
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return <h1>Hello World</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
